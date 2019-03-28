65720d943d256db530d01c04d31e3476cb1dbaf2
Laura Oliveira <laura.regina.cp@gmail.com>
2019-03-28 16:22:20 -0300

    Tela para escolher entre estudar algo novo ou cadastrar serviço

---
+++ app/src/main/java/com/empodera/ServiceOrStudyActivity.java
@@ -0,0 +1,13 @@
+package com.empodera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceOrStudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_or_study);
+    }
+}
