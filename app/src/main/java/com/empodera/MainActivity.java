8131343fd1e483be9301946828fff01047cabb84
Laura Oliveira <laura.regina.cp@gmail.com>
2019-03-28 22:57:01 -0300

    Implementação do fluxo principal do sistema

--- app/src/main/java/com/empodera/MainActivity.java
+++ app/src/main/java/com/empodera/MainActivity.java
@@ -1,7 +1,9 @@
 package com.empodera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registerService(View view)
+    {
+        Intent registerServiceActivityObject = new Intent(this, RegisterServiceActivity.class);
+        startActivity(registerServiceActivityObject);
+    }
+
+    public void searchWorker(View view)
+    {
+        Intent searchWorkerActivity = new Intent(this, SearchWorkerActivity.class);
+        startActivity(searchWorkerActivity);
+    }
+
+    public void learnSubject(View view)
+    {
+        Intent learnSubjectActivityObject = new Intent(this, LearnSubjectActivity.class);
+        startActivity(learnSubjectActivityObject);
     }
 }
--- app/src/main/java/com/empodera/SearchWorkerActivity.java
+++ app/src/main/java/com/empodera/SearchWorkerActivity.java
@@ -3,11 +3,11 @@ package com.empodera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ServiceOrStudyActivity extends AppCompatActivity {
+public class SearchWorkerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_or_study);
+        setContentView(R.layout.activity_search_worker);
     }
 }
