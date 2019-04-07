40f0c55bdae6f716fefc8178d3fe30a550550563
Laura-Oliveira <laura.regina.cp@gmail.com>
2019-04-07 00:36:32 -0300

    Implementação da Tela de Aprender algo novo

--- app/src/main/java/com/empodera/LearnSubjectActivity.java
+++ app/src/main/java/com/empodera/LearnSubjectActivity.java
@@ -2,6 +2,8 @@ package com.empodera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class LearnSubjectActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LearnSubjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_subject);
+
+        TextView txtLinkMarmitex = findViewById(R.id.link_marmitex);
+        txtLinkMarmitex.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView txtLinkSummer = findViewById(R.id.link_summer);
+        txtLinkSummer.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView txtLinkRevendaProdutos = findViewById(R.id.link_revenda_produtos);
+        txtLinkRevendaProdutos.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
